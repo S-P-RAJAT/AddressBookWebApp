cd2f3185297198584ce959b1055e8b5008dc52a3
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-10-21 03:55:51 +0530

    ADDED: Address Book json file and its JSON server url

--- js/site_properties.js
+++ js/site_properties.js
@@ -2,4 +2,5 @@ let site_properties = {
     use_local_storage: "true",
     home_page: "../pages/home.html",
     add_contact_page:"../pages/form.html",
+    server_url: "http://localhost:3000/addressBookDB/"
 }
